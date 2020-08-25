5da9edb2a5b1d5ae5315c93d51390c59b8874c05
rustikelli <mehdizaderustam@gmail.com>
2020-08-24 23:10:59 -0400

    added 3.1 3.2

--- lesson_3/homework_3_1.py
+++ lesson_3/homework_3_1.py
@@ -3,9 +3,13 @@
 # Ex. 1
 # Enter your name, save it in name variable and save in result_1 variable your name repeated 3 times (use loops)
 
-name_1 = None
-result_1 = None
+name_1 = "Rustam"
+result_1 = ""
 
+for counter in range(1, 4):
+    result_1 += name_1
+
+print(result_1)
 # TODO: Here is your code
 
 
@@ -13,9 +17,16 @@ result_1 = None
 # Modify your previous program so that it will enter your name (save it in variable  name_2) and a number
 # (save in variable number) and then save in result_2 variable your name repeated as many times as number_1 is
 # (use loops)
-name_2 = None
-number_1 = None
-result_2 = None
+name_2 = "Rustam"
+result_2 = ""
+number_1 = 5
+
+for counter in range(number_1):
+    result_2 += name_2
+
+print(result_2)
+
+
 
 # TODO: Here is your code
 
@@ -24,8 +35,14 @@ result_2 = None
 # Enter a random string, which includes only digits. Write code which find a sum of digits in this string and save it
 # into result_3 variable
 
-string_number_1 = None
-result_3 = None
+string_number_1 = "12345"
+result_3 = 0
+
+for char in string_number_1:
+    result_3 += int(char)
+
+print(result_3)
+
 
 # TODO: Here is your code
 
@@ -33,6 +50,11 @@ result_3 = None
 # Ex. 4
 # Create code which sums up all even numbers between 2 and 100 (include 100) and save it in result_4 variable
 
-result_4 = None
+result_4 = 0
+
+for even_numbers in range(2, 101, 2):
+    result_4 += even_numbers
+
+print(result_4)
 
 # TODO: Here is your code
--- lesson_3/homework_3_2.py
+++ lesson_3/homework_3_2.py
@@ -4,9 +4,19 @@
 # Write code, which will count how many times your character is included in your string.
 # Save result to result_1 variable
 
-string_1 = None
-char_1 = None
-result_1 = None
+string_1 = "My name is Rustam"
+char_1 = "m"
+result_1 = 0
+counter = 0
+
+while counter < len(string_1):
+    if string_1 [counter] == char_1:
+        result_1 += 1
+    counter += 1
+
+print(result_1)
+
+
 
 
 # Enter a random number and save it in variable number_1. Then create code to multiply all the digits together
